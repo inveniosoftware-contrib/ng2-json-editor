8512dd455c060db7fe99af119445a01f25e983b2
harunurhan <harunurhan17@gmail.com>
2016-10-19 15:44:22 +0200

    change from ngOnChanges to ngOnInit

--- src/json-editor.component.ts
+++ src/json-editor.component.ts
@@ -25,7 +25,7 @@ import {
   EventEmitter,
   Input,
   Output,
-  OnChanges,
+  OnInit,
   ViewEncapsulation
 } from '@angular/core';
 import { Http } from '@angular/http';
@@ -47,7 +47,7 @@ import {
   ],
   templateUrl: './json-editor.component.html'
 })
-export class JsonEditorComponent extends AbstractTrackerComponent implements OnChanges {
+export class JsonEditorComponent extends AbstractTrackerComponent implements OnInit {
 
   @Input() record: Object;
   @Input() schema: Object;
@@ -75,11 +75,7 @@ export class JsonEditorComponent extends AbstractTrackerComponent implements OnC
     return this.componentTypeService.getComponentType(fieldScehma);
   }
 
-  // FIXME: called two times!
-  ngOnChanges() {
-    // TODO: remove this when this is called only once.
-    if (Object.keys(this.record).length === 0) { return; }
-
+  ngOnInit() {
     this.record = this.recordFixerService.fixRecord(this.record, this.schema);
     this.previews = this.extractPreviews();
     this.appGlobalsService.globalErrors = this.errorMap;
