7abf0a2fb95bbd1f00bb382d5cfd9ab0cdde85b8
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2016-11-16 09:56:05 +0100

    wrap-content: Fix for issue #72.
     * Creates a new shared directive `autofit` which resize each textarea element
       to it's content height respectively.
    
    Signed-off-by: Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,7 +1,9 @@
 import { SHARED_PIPES } from './pipes';
 import { SHARED_SERVICES } from './services';
+import { SHARED_DIRECTIVES } from './directives';
 
 export {
   SHARED_PIPES,
-  SHARED_SERVICES
+  SHARED_SERVICES,
+  SHARED_DIRECTIVES
 };
