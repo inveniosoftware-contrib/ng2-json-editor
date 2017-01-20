a95a1cd3f1d2b275a4e8369579a286f9e581bbe9
inspirehep <admin@inspirehep.net>
2017-01-20 13:22:57 +0100

    Add yarn.lock

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+ng build --output-path="example-dist" --base-href="https://inveniosoftware-contrib.github.io/ng2-json-editor/"
+
+echo 'hello'
+echo $GH_TOKEN
+git clone -b gh-pages --single-branch https://${GH_TOKEN}@${GH_REF} gh-pages
+git remote -v
+rm -rf gh-pages/*.js gh-pages/*.map  gh-pages/assets
+cp -r example-dist/* gh-pages
+cd gh-pages
+
 git config user.name "inspirehep"
 git config user.email "admin@inspirehep.net"
 
-npm run build && ng github-pages:deploy
+# add and commit
+git add .
+git commit -m "deployment to github pages"
+
+git push origin gh-pages
